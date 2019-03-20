f3bfe4c70818ae88ef1333ee82ebe5dfe9800c35
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-03-20 16:13:29 -0600

    Added debugging to samples

--- src/main/kotlin/SampleWithDebugging.kt
+++ src/main/kotlin/SampleWithDebugging.kt
@@ -1,26 +1,24 @@
-package sample
+package sampleDebugging
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 //-Dkotlinx.coroutines.debug
 
 fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
 
 fun main() = runBlocking {
-    val aFuture = async {
+    val aFuture = async(Dispatchers.Default) {
         delay(3000)
         log("1")
         "a"
     }
-    val bFuture = async {
+    val bFuture = async(Dispatchers.Default) {
         delay(2000)
         log("2")
         log(aFuture.await())
         "b"
     }
-    val cFuture = async {
+    val cFuture = async(Dispatchers.Default) {
         delay(1000)
         log("3")
         log(bFuture.await())
